10d264759bd409011884fd4b041d54b7e57e30d5
Anibal Rojas <anibal@wisedigitalpartners.com>
2024-10-18 03:54:31 -0500

    On the horizon fixes

--- web/src/pages/on-the-horizon.js
+++ web/src/pages/on-the-horizon.js
@@ -12,7 +12,7 @@ const Page = ({ data }) => {
     <Layout>
       <SearchEngineOptimization
         title="How to Improve Cooling Tower Efficiency: ALI Water Recovery"
-        description="How can you improve cooling tower efficiency? ALI by Breakthrough Technologies is a new water recovery solution for cooling towers. Learn more here."
+        description="How can you improve cooling tower efficiency? ALI by Nouvel Technologies is a new water recovery solution for cooling towers. Learn more here."
         openGraphImage={data.openGraphImage.publicURL}
         twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
       />
@@ -22,18 +22,17 @@ const Page = ({ data }) => {
           <header className="mb-14 md:mb-20 max-w-4xl">
             <h1>On The Horizon</h1>
             <p className="mb-0">
-              Breakthrough Technologies maintains a number of groundbreaking
-              patented and patent-pending technologies that improve
-              sustainability and drive better business performance. Below are a
-              number of new industrial solutions that Breakthrough Technologies
-              is on the cusp of bringing to market. For more information or
-              questions about these industrial solutions on the horizon, please
-              contact us at{" "}
+              Nouvel Technologies maintains a number of groundbreaking patented
+              and patent-pending technologies that improve sustainability and
+              drive better business performance. Below are a number of new
+              industrial solutions that Nouvel Technologies is on the cusp of
+              bringing to market. For more information or questions about these
+              industrial solutions on the horizon, please contact us at{" "}
               <a
-                href="mailto:info@bt-tech.com"
+                href="mailto:info@nouveltechnologies.com"
                 className="underline whitespace-nowrap"
               >
-                info@bt-tech.com
+                info@nouveltechnologies.com
               </a>
               .
             </p>
@@ -44,17 +43,17 @@ const Page = ({ data }) => {
               <h2 className="mb-2.5">SulphyPro</h2>
               <div className="bg-primary-400 w-16 h-1 mb-6"></div>
               <p className="mb-0">
-                Hydrogen sulfide is a naturally occurring, yet toxic and
+                Hydrogen sulfide is a naturally occurring yet toxic and
                 corrosive impurity that is produced from decaying organic
-                matter. It is created in the process of refining petroleum and
-                it is also released during the fermentation of organic material
-                to make biogas. Instead of using a hazardous and expensive
-                purification process, Breakthrough Technologies’ innovative
-                SulphyPro uses a combination of microwaves and manipulated
-                ultraviolet-light waves to excite and separate hydrogen sulfide
-                molecules into hydrogen and sulfur. This nascent recycled
-                hydrogen can save operating costs, be burned for energy or
-                tapped as a renewable transportation source.
+                matter. It is created in the process of refining petroleum also
+                released during the fermentation of organic material to make
+                biogas. Instead of using a hazardous and expensive purification
+                process, Nouvel Technologies’ innovative SulphyPro uses a
+                combination of microwaves and manipulated ultraviolet-light
+                waves to excite and separate hydrogen sulfide molecules into
+                hydrogen and sulfur. This nascent recycled hydrogen can save
+                operating costs, be burned for energy, or be tapped as a
+                renewable transportation source.
               </p>
             </div>
 
@@ -78,20 +77,20 @@ const Page = ({ data }) => {
               </h2>
               <div className="bg-primary-400 w-16 h-1 mb-6"></div>
               <p className="mb-0">
-                Second only to water, concrete is the most widely-used substance
+                Second only to water, concrete is the most widely used substance
                 on Earth. The process of making cement, the key ingredient in
                 concrete, emits a significant amount of man-made waste heat,
                 which is a major sustainability issue. To combat this global
-                problem, Breakthrough Technologies created the Phoenix Pressure
-                and Heat Exchanger (PHX), which involves a series of
-                patent-pending thermoelectric generators or Breakthrough Cubes
-                that when exposed to heat create electricity for everyday use.
-                From a standard sized kiln, Phoenix can create 20MW of
-                electricity, which is enough to power 10,000 homes and worth
-                about $12.3 million per year. The process reduces the need to
-                use carbon-intense energy sources and reuses waste heat. Future
+                problem, Nouvel Technologies created the Phoenix Pressure and
+                Heat Exchanger (PHX), which involves a series of patent-pending
+                thermoelectric generators or Breakthrough Cubes that, when
+                exposed to heat, create electricity for everyday use. From a
+                standard-sized kiln, Phoenix can create 20MW of electricity,
+                which is enough to power 10,000 homes and is worth about $12.3
+                million per year. The process reduces the need for
+                carbon-intense energy sources and reuses waste heat. Future
                 applications of Phoenix will include air conditioning units,
-                steel mills and other industrial processes involving man-made
+                steel mills and other industrial processes that involve man-made
                 waste heat.
               </p>
             </div>
@@ -102,17 +101,17 @@ const Page = ({ data }) => {
               <h2 className="mb-2.5">Multi-Effect Process (MEP)</h2>
               <div className="bg-primary-400 w-16 h-1 mb-6"></div>
               <p className="mb-0">
-                It is estimated that over 18 trillion gallons of wastewater is
-                generated in North America every year. Breakthrough
-                Technologies’ groundbreaking Multi-Effect Process uses a novel
-                combination of thermo-electrochemistry techniques to reduce the
-                organic (COD, BOD) and inorganic (TDS, etc.) impurities in
-                wastewater. This innovative process not only makes wastewater
-                recyclable for industrial processes, but it also improves on an
-                expensive method, the reverse-osmosis process, which requires
-                significant funding, maintenance and energy. MEP allows users to
-                reclaim water in industrial processes applicable to cane sugar,
-                bio-digesters, textiles, fats and oils and petrochemicals. The
+                It is estimated that over 18 trillion gallons of wastewater are
+                generated in North America every year. Nouvel Technologies’
+                groundbreaking Multi-Effect Process uses a novel combination of
+                thermo-electrochemistry techniques to reduce the organic (COD,
+                BOD) and inorganic (TDS, etc.) impurities in wastewater. This
+                innovative process not only makes wastewater recyclable for
+                industrial processes but also improves on an expensive method
+                (the reverse-osmosis process), which requires significant
+                funding, maintenance, and energy. MEP allows users to reclaim
+                water in industrial processes applicable to cane sugar,
+                bio-digesters, textiles, fats, oils, and petrochemicals. The
                 system also allows users to reclaim remaining solids as
                 fertilizer and gas as energy. With MEP, a typical midsize
                 industrial site can reclaim enough water to save a million
